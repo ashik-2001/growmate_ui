3d8cab1104a87d20db275f68efd8ca14107e2fd0
Ashik-2001 <ashikbabu2001@gmail.com>
2022-09-19 22:03:21 +0530

    Upper UI completed

--- lib/upper_portion/upper_portion.dart
+++ lib/upper_portion/upper_portion.dart
@@ -8,30 +8,45 @@ class UpperPortion extends StatelessWidget {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
     const maxline = 2;
-    return Container(
-      width: width,
-      height: height * 0.4,
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.fill,
-          image: AssetImage('assets/Vector.png'),
+    return Stack(children: [
+      Container(
+        width: width,
+        height: height * 0.4,
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: AssetImage('assets/Vector.png'),
+          ),
+          gradient: LinearGradient(
+              begin: Alignment.center,
+              end: Alignment.topCenter,
+              colors: [Colors.black, Colors.white]),
         ),
       ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.end,
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          IconButton(
-            onPressed: () {},
-            icon: Image.asset('assets/icons/backB.png'),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
+      Container(
+        width: width,
+        height: height * 0.4,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+              begin: Alignment.bottomCenter,
+              end: Alignment.topCenter,
+              colors: [Color.fromARGB(255, 27, 26, 26), Colors.transparent]),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.end,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            IconButton(
+              onPressed: () {},
+              icon: Image.asset('assets/icons/backB.png'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(18.0,0,0,8.0),
+                  padding: const EdgeInsets.fromLTRB(18.0, 0, 0, 8.0),
                   child: SizedBox(
-                    width: width*0.8,
+                    width: width * 0.8,
                     // ignore: prefer_const_constructors
                     child: Text(
                       maxLines: 2,
@@ -46,13 +61,17 @@ class UpperPortion extends StatelessWidget {
                   ),
                 ),
                 Padding(
-                  padding: const EdgeInsets.fromLTRB(0,0,5.0,0),
-                  child: IconButton(onPressed: (){}, icon: Image.asset('assets/icons/settings.png')),
-                )
-            ],
-          )
-        ],
+                  padding: const EdgeInsets.fromLTRB(0, 0, 5.0, 0),
+                  child: IconButton(
+                    onPressed: () {},
+                    icon: Image.asset('assets/icons/settings.png'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
-    );
+    ]);
   }
 }
