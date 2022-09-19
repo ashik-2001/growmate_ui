b58d3c29597399524f9513fbd219ec46ab4ba053
Ashik-2001 <ashikbabu2001@gmail.com>
2022-09-19 21:52:41 +0530

    WIP: Added texts and buttons in upper part

--- lib/upper_portion/upper_portion.dart
+++ lib/upper_portion/upper_portion.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class UpperPortion extends StatelessWidget {
   const UpperPortion({Key? key}) : super(key: key);
 
@@ -8,14 +7,52 @@ class UpperPortion extends StatelessWidget {
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
     final width = MediaQuery.of(context).size.width;
+    const maxline = 2;
     return Container(
-              height: height * 0.4,
-              decoration: const BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.fill,
-                  image: AssetImage('assets/Vector.png'),
+      width: width,
+      height: height * 0.4,
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.fill,
+          image: AssetImage('assets/Vector.png'),
+        ),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.end,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset('assets/icons/backB.png'),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(18.0,0,0,8.0),
+                  child: SizedBox(
+                    width: width*0.8,
+                    // ignore: prefer_const_constructors
+                    child: Text(
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      'Will China invade Taiwan before end september? ',
+                      style: const TextStyle(
+                        fontSize: 22,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'lato',
+                      ),
+                    ),
+                  ),
                 ),
-              ),
-            );
+                Padding(
+                  padding: const EdgeInsets.fromLTRB(0,0,5.0,0),
+                  child: IconButton(onPressed: (){}, icon: Image.asset('assets/icons/settings.png')),
+                )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
