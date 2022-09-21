40ac6ae260580b1f450553ed41743f3b87c88e02
Ashik-2001 <ashikbabu2001@gmail.com>
2022-09-21 15:30:43 +0530

    Completed reactions tab

--- lib/reactions/reactions.dart
+++ lib/reactions/reactions.dart
@@ -5,9 +5,12 @@ class Reactions extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
     return Padding(
       padding: const EdgeInsets.fromLTRB(0,0,8,0),
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -43,7 +46,88 @@ class Reactions extends StatelessWidget {
               ),
             ],
           ),
-          
+          SizedBox(height: 20,),
+          Row(
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset('assets/picbg1.png'),
+                  Image.asset('assets/pic1.png')
+                ],
+              ),
+              SizedBox(width: 10,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text('Linda ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),),
+                      Text('If China does indeed attack Taiwan soon,',style: TextStyle(fontSize: 13)),
+                    ],
+                  ),
+                  
+                  Row(
+                    children: [
+                      Text('October is the likely time since the...',style: TextStyle(fontSize: 13)),
+                      Text('more',style: TextStyle(fontSize: 13, color: Colors.grey),)
+                    ],
+                  ),
+                ],
+              ),
+            
+            ],
+          ),
+          SizedBox(height: 10),
+          Row(
+            children: [
+              Stack(
+                alignment: Alignment.center,
+                children: [
+                  Image.asset('assets/picbg2.png'),
+                  Image.asset('assets/pic2.png')
+                ],
+              ),
+              SizedBox(width: 10,),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    children: [
+                      Text('TomBN ', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),),
+                      Text('I will be surprised if #China will in fact',style: TextStyle(fontSize: 13)),
+                    ],
+                  ),
+                  
+                  Row(
+                    children: [
+                      Text('attack Taiwan. Doing so requires lots of... ',style: TextStyle(fontSize: 13)),
+                      Text('more',style: TextStyle(fontSize: 13, color: Colors.grey),)
+                    ],
+                  ),
+                ],
+              ),
+            
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              SizedBox(
+                width: width/2,
+                child: TextField(
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Add comment...'
+                  ),
+                ),
+              ),
+              IconButton(onPressed: (){}, icon: Image.asset('assets/icons/eyelove.png')),
+              IconButton(onPressed: (){}, icon: Image.asset('assets/icons/cry.png')),
+              IconButton(onPressed: (){}, icon: Image.asset('assets/icons/plus.png')),
+              
+            ],
+          ),
         ],
       ),
     );
